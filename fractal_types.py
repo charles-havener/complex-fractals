8945757dba5265b96a75289e6a809b6dab42a7de
Charles Havener <charles.havener@gmail.com>
2022-01-06 15:42:41 -0700

    removed subclasses and instead went with an 'identifier' argument that determines the fractal type to create. A new file with dicts of parameters and possible identifier names was added to initialize fractal type specific variables when creating images

---
+++ fractal_types.py
@@ -0,0 +1,89 @@
+f_type_identifiers = {
+    0 : 0,
+    "mandelbrot" : 0,
+    1 : 1,
+    "mandelbar" : 1,
+    2 : 2,
+    "perpendicular_mandelbrot" : 2,
+    3 : 3,
+    "celtic_mandelbrot" : 3,
+    4 : 4,
+    "celtic_mandelbar" : 4,
+    5 : 5,
+    "perpendicular_celtic" : 5,
+    6 : 6,
+    "burning_ship" : 6,
+    7 : 7,
+    "heart_mandelbrot" : 7,
+    8 : 8,
+    "perpendicular_burning_shipe" : 8,
+    9 : 9,
+    "buffalo" : 9,
+    10 : 10,
+    "heart_celtic" : 10,
+    11 : 11,
+    "perpendicular_buffalo" : 11,
+}
+
+f_type_parameters = {
+    0 : { # Mandelbrot
+        "im_range" : 2.5,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "mandelbrot",
+    },
+    1 : { # Mandelbar
+        "im_range" : 5.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "mandelbar",
+    },
+    2 : { # Perpendicular Mandelbrot
+        "im_range" : 4.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "perpendicular_mandelbrot",
+    },
+    3 : { # Celtic Mandelbrot
+        "im_range" : 4.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "celtic_mandelbrot",
+    },
+    4 : { # Celtic Mandelbar
+        "im_range" : 2.5,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "celtic_mandelbar",
+    },
+    5 : { # Perpendicular Celtic
+        "im_range" : 3.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "perpendicular_celtic",
+    },
+    6 : { # Burning Ship
+        "im_range" : 3.0,
+        "center" : complex(-0.3775, 0.5),
+        "filename" : "burning_ship",
+    },
+    7 : { # Heart Mandelbrot
+        "im_range" : 3.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "heart_mandelbrot",
+    },
+    8 : { # Perpendicular Burning Ship
+        "im_range" : 3.0,
+        "center" : complex(-0.3775, 0.0),
+        "filename" : "perpendicular_burning_ship",
+    },
+    9 : { # Buffalo
+        "im_range" : 3.0,
+        "center" : complex(-0.70, 0.5),
+        "filename" : "buffalo",
+    },
+    10 : { # Heart Celtic
+        "im_range" : 2.0,
+        "center" : complex(-0.5, 0.0),
+        "filename" : "heart_celtic",
+    },
+    11 : { # Perpendicular Buffalo
+        "im_range" : 3.0,
+        "center" : complex(-0.5, 0.0),
+        "filename" : "perpendicular_buffalo",
+    },
+}
